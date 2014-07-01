948802d3e13a7e437a589031f8143ece94612b30
Stephan <stephane@umbraco.dk>
2014-07-01 11:20:18 +0200

    Create NuGet package

--- Zbu.Yol/Properties/CommonInfo.cs
+++ Zbu.Yol/Properties/CommonInfo.cs
@@ -10,5 +10,7 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: AssemblyInformationalVersion("1.0.0-alpha001")]
+// Do not remove this line
